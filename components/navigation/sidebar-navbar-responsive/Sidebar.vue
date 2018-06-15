daa7fa9511a7acb568dc1f6799277f242ce69159
Bennett <dams.bennett@gmail.com>
2018-06-15 14:38:15 +0200

    Create Sidebar.vue

---
+++ components/navigation/sidebar-navbar-responsive/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <div id="sidebar"> -->
+
+      <div class="py-2 sticky-top">
+
+        <a class="navbar-brand mx-0 px-0" href="#">
+          <img class="img-fluid" src="">
+        </a>
+
+        <div class="nav flex-column">
+          <NavigationItems></NavigationItems>
+        </div>
+
+      </div>
+
+
+  <!-- </div> -->
+  </template>
+
+  <script>
+  import NavigationItems from './NavigationItems.vue'
+  export default {
+    name: 'Sidebar',
+    components: {
+      NavigationItems
+    }
+  }
+  </script>
+
+  <style scoped>
+  </style>
