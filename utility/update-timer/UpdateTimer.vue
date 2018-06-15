3c538f41406f4f692526d216360331d05951c4e2
Bennett <dams.bennett@gmail.com>
2018-06-15 13:32:43 +0200

    Create UpdateTimer.vue

---
+++ utility/update-timer/UpdateTimer.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="update-timer">
+  <small class="text-muted">Last updated {{ lastUpdated }}</small>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'UpdateTimer',
+  props: {
+    updateRate: Number,
+  },
+  data() {
+    return {
+      timerSeconds: 0,
+      lastUpdated: ''
+    }
+  },
+  mounted() {
+    this.updateTimer();
+  },
+  methods: {
+    updateTimer() {
+      window.setInterval(() => {
+        this.timerSeconds++;
+        if (this.timerSeconds <= 5) {
+          this.lastUpdated = "right now."
+        } else {
+          this.lastUpdated = this.timerSeconds + " seconds ago";
+        }
+        if (this.timerSeconds >= this.updateRate) {
+          this.timerSeconds = 0;
+        }
+      }, 1000);
+    }
+  }
+}
+</script>
+
+<style src="./css/UpdateTimer.css" scoped>
+</style>
