9caf91b0bbbac1d89d2bbe71e94a29675b2e2ef0
Bennett <dams.bennett@gmail.com>
2018-06-15 14:42:44 +0200

    Create NavbarMobile.vue

---
+++ components/navigation/sidebar-navbar-responsive/NavbarMobile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="navbar-mobile">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">MY WEBSITE</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarToggler">
+
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <NavigationItems></NavigationItems>
+        </ul>
+
+      </div>
+    </nav>
+
+
+  </div>
+  </template>
+
+  <script>
+  import NavigationItems from './NavigationItems.vue'
+  export default {
+    name: 'NavbarMobile',
+    components: {
+      NavigationItems
+    }
+  }
+  </script>
+
+  <style scoped>
+  </style>
